3f9964d3a288149720f0479328d44f333cc861f9
QuiLion7 <quilbrub@gmail.com>
2023-01-29 23:06:52 -0300

    1º Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center"> QuiLinks </h1>
+
+<p align="center">
+QuiLinks é um agregador de links para usar como cartão de visitas online.<br/>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-diferencial">Diferencial</a>
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+## 💻 Projeto
+
+O projeto foi promovido pela Rocketseat para ensino de tecnologias WEB.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/community/file/
+
+## 🧑‍💻 Diferencial
+
+- JavaScript reescrito e refatorado
+- Armazenamento do tema em LocalStorage
+- [Acesse o projeto finalizado, online](https://quilion7.github.io/quilinks/)
+
+---
+
+Desenvolvido por Quilion Oliveira
---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.querySelector('body')
+const switchTheme = document.querySelector('[data-js="switch-theme"]')
+const imgMyProfile = document.querySelector('[data-js="img-my-profile"]')
+
+const checkThemeLocalStorage = () => {
+  const themeIsLightInLocalStorage = JSON.parse(localStorage.getItem('theme')) === 'light'
+  
+  if(themeIsLightInLocalStorage) {
+    body.classList.add('light')
+  }
+  
+  changeMyProfile()
+}
+
+const changeMyProfile = () => {
+  body.classList.contains('light') 
+  ? imgMyProfile.setAttribute("src", "./assets/avatar-light.png")
+  : imgMyProfile.setAttribute("src", "./assets/avatar.png")
+}
+
+const changeTheme = () => {
+  const themeIsLight = body.classList.toggle('light')
+  saveLocalStorage(themeIsLight)
+  changeMyProfile()
+}
+const setItemLocalStorage = theme => localStorage.setItem('theme',JSON.stringify(theme))
+
+const saveLocalStorage = isLight => isLight ? setItemLocalStorage('light') : setItemLocalStorage('dark')
+
+switchTheme.addEventListener('click', changeTheme)
+
+window.addEventListener('load', checkThemeLocalStorage)
